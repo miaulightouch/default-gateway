5b87791e7e279427844b601fcd9f569b15fa3c77
Billy Song <billy@example.com>
2022-01-28 20:38:16 +0800

    fix: require logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const {platform, type} = require("os");
+const { type } = require("os");
 
 const supportedPlatforms = new Set([
   "aix",
@@ -13,21 +13,25 @@ const supportedPlatforms = new Set([
   "win32"
 ]);
 
-const plat = platform();
+if (supportedPlatforms.has(process.platform)) {
+  const m = (() => {
+    if (process.platform === 'aix') {
+      if (type() === 'OS400') {
+        require('./ibmi');
+      } else {
+        require('./sunos');
+      }
+    } else {
+      require(`./${process.platform}`);
+    }
+  })();
 
-if (supportedPlatforms.has(plat)) {
-  let file = plat;
-  if (plat === "aix") {
-    file = type() === "OS400" ? "ibmi" : "sunos"; // AIX `netstat` output is compatible with Solaris
-  }
-
-  const m = require(`./${file}.js`);
   module.exports.v4 = () => m.v4();
   module.exports.v6 = () => m.v6();
   module.exports.v4.sync = () => m.v4.sync();
   module.exports.v6.sync = () => m.v6.sync();
 } else {
-  const err = new Error(`Unsupported Platform: ${plat}`);
+  const err = new Error(`Unsupported Platform: ${process.platform}`);
   module.exports.v4 = () => Promise.reject(err);
   module.exports.v6 = () => Promise.reject(err);
   module.exports.v4.sync = () => { throw err; };
